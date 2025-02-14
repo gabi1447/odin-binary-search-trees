208fe40fce26df32b8ba92024c5fb41770c24713
Gabriel <gabrielpachecotec@gmail.com>
2025-02-14 13:17:34 +0000

    Write Tree factory function and buildTree function
    The buildTree function takes a sortedArray and then builds a balanced
    binary search tree returning the root of the newly built balanced tree.

--- bst.js
+++ bst.js
@@ -1,3 +1,5 @@
+import { mergeSort } from "./mergeSort.js";
+
 function Node(value) {
     const data = value;
     let left = null;
@@ -7,5 +9,67 @@ function Node(value) {
         data,
         left,
         right,
+    };
+}
+
+function Tree(array) {
+    let sortedArray = sortUnorderedArray(array);
+    let root = buildTree(sortedArray, 0, sortedArray.length - 1);
+
+    function getRoot() {
+        return root;
+    }
+
+    function sortUnorderedArray(array) {
+        const arrayWithoutDuplicates = removeDuplicates(array);
+        const sortedArray = mergeSort(arrayWithoutDuplicates);
+        return sortedArray;
+    }
+
+    function removeDuplicates(array) {
+        const setWithoutDuplicates = new Set(array);
+        const arrayWithoutDuplicates = Array.from(setWithoutDuplicates);
+
+        return arrayWithoutDuplicates;
+    }
+
+    function buildTree(sortedArray, start, end) {
+        if (start > end) {
+            return null;
+        }
+
+        const mid = Math.floor((start + end) / 2);
+        const root = Node(sortedArray[mid]);
+
+        root.left = buildTree(sortedArray, start, mid - 1);
+        root.right = buildTree(sortedArray, mid + 1, end);
+
+        return root;
+    }
+
+    function prettyPrint(node, prefix = "", isLeft = true) {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            prettyPrint(
+                node.right,
+                `${prefix}${isLeft ? "│   " : "    "}`,
+                false
+            );
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+            prettyPrint(
+                node.left,
+                `${prefix}${isLeft ? "    " : "│   "}`,
+                true
+            );
+        }
+    }
+
+    return {
+        prettyPrint,
+        getRoot,
     };
 }
